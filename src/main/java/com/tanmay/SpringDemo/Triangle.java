6f73dc19408e34eb4cdd49518f6016b46a31d600
Tanmay Rajani <rajani.tanmay@gmail.com>
2016-02-12 13:16:48 +0530

    added constructor injection files

--- src/main/java/com/tanmay/SpringDemo/Triangle.java
+++ src/main/java/com/tanmay/SpringDemo/Triangle.java
@@ -2,16 +2,27 @@ package com.tanmay.SpringDemo;
 
 public class Triangle {
     private String type;
+    private int height;
+
+    public Triangle(String type, int height) {
+        this.type = type;
+        this.height = height;
+    }
+
+    public int getHeight() {
+        return height;
+    }
 
     public String getType() {
         return type;
+
     }
 
-    public void setType(String type) {
-        this.type = type;
-    }
+//    public void setType(String type) {
+//        this.type = type;
+//    }
 
     public void draw(){
-        System.out.println(getType()+" Triangle Drawn");
+        System.out.println(getType()+" Triangle Drawn of height "+getHeight());
     }
 }
